c88255e5daec21a568777a3fd7b6dde6949d8527
ambrose214 <ambrose.kuo@hotmail.com>
2019-01-07 16:37:37 -0500

    zz

--- src/App.js
+++ src/App.js
@@ -1,28 +1,225 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+
+const axiosGitHubGraphQL = axios.create({
+  baseURL: "https://api.github.com/graphql",
+  headers: {
+    Authorization: `bearer ${
+      process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN
+    }`
+  }
+});
+
+const GET_ORGANIZATION = `
+  {
+    organization(login: "the-road-to-learn-react") {
+      name
+      url
+    }
+  }
+`;
+
+const GET_REPOSITORY_OF_ORGANIZATION = `
+  {
+    organization(login: "the-road-to-learn-react") {
+      name
+      url
+      repository(name: "the-road-to-learn-react") {
+        name
+        url
+      }
+    }
+  }
+`;
+
+const GET_ISSUES_OF_REPOSITORY = `
+  query ($organization: String!, $repository: String!, $cursor: String) {
+    organization(login: $organization) {
+      name
+      url
+      repository(name: $repository) {
+        name
+        url
+        issues(first: 5, after: $cursor, states: [OPEN]) {
+          edges {
+            node {
+              id
+              title
+              url
+              reactions(last: 3) {
+                edges {
+                  node {
+                    id
+                    content
+                  }
+                }
+              }
+            }
+          }
+          totalCount
+          pageInfo {
+            endCursor
+            hasNextPage
+          }
+        }
+      }
+    }
+  }
+`;
+
+const getIssuesOfRepositoryQuery = (organization, repository) => `
+  {
+    organization(login: "${organization}") {
+      name
+      url
+      repository(name: "${repository}") {
+        name
+        url
+        issues(last: 5) {
+          edges {
+            node {
+              id
+              title
+              url
+            }
+          }
+        }
+      }
+    }
+}
+`;
+
+const getIssuesOfRepository = path => {
+  const [organization, repository] = path.split("/");
+
+  return axiosGitHubGraphQL.post("", {
+    query: GET_ISSUES_OF_REPOSITORY,
+    variables: { organization, repository }
+  });
+};
+
+const resolveIssuesQuery = queryResult => () => ({
+  organization: queryResult.data.data.organization,
+  errors: queryResult.data.errors
+});
+
+const TITLE = "React GraphQL GitHub Client";
 
 class App extends Component {
+  state = {
+    path: "the-road-to-learn-react/the-road-to-learn-react",
+    organization: null,
+    errors: null
+  };
+
+  componentDidMount() {
+    this.onFetchFromGitHub(this.state.path);
+  }
+
+  onChange = event => {
+    this.setState({ path: event.target.value });
+  };
+
+  onSubmit = event => {
+    //fetch data
+    this.onFetchFromGitHub(this.state.path);
+    event.preventDefault();
+  };
+
+  onFetchFromGitHub = (path, cursor) => {
+    getIssuesOfRepository(path, cursor).then(queryResult =>
+      this.setState(resolveIssuesQuery(queryResult, cursor))
+    );
+  };
+
+  onFetchMoreIssues = () => {
+    const {
+      endCursor,
+    } = this.state.organization.repository.issues.pageInfo;
+
+    this.onFetchFromGitHub(this.statepath.endCursor);
+  };
+
   render() {
+    const { path, organization, errors } = this.state;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1> {TITLE} </h1>
+
+        <form onSubmit={this.onSubmit}>
+          <label htmlFor="url">Show open issues for https://github.com/</label>
+          <input
+            id="url"
+            type="text"
+            value={path}
+            onChange={this.onChange}
+            style={{ with: "300px" }}
+          />
+          <button type="submit"> Search </button>
+        </form>
+
+        <hr />
+        {organization ? (
+          <Organization
+            organization={organization}
+            errors={errors}
+            onFetchMoreIssues={this.onFetchMoreIssues}
+          />
+        ) : (
+          <p> No information yet ... </p>
+        )}
       </div>
     );
   }
 }
+
+const Organization = ({ organization, errors, onFetchMoreIssues }) => {
+  if (errors) {
+    return (
+      <p>
+        <strong> Something went Wrong </strong>
+        {errors.map(error => error.message).join(" ")}
+      </p>
+    );
+  }
+
+  return (
+    <div>
+      <p>
+        <strong>Issues from Organization:</strong>
+        <a href={organization.url}>{organization.name}</a>
+      </p>
+      <Repository
+        repository={organization.repository}
+        onFetchMoreIssues={onFetchMoreIssues}
+      />
+    </div>
+  );
+};
+
+const Repository = ({ repository, onFetchMoreIssues }) => (
+  <div>
+    <p>
+      <strong>In Repository:</strong>
+      <a href={repository.url}>{repository.name}</a>
+    </p>
+
+    <ul>
+      {repository.issues.edges.map(issue => (
+        <li key={issue.node.id}>
+          <a href={issue.node.url}>{issue.node.title}</a>
+          <ul>
+            {issue.node.reactions.edges.map(reaction => (
+              <li key={reaction.node.id}>{reaction.node.content}</li>
+            ))}
+          </ul>
+        </li>
+      ))}
+    </ul>
+    <hr />
+    <button onClick={onFetchMoreIssues}>More</button>
+  </div>
+);
 
 export default App;
